163ca04afd59311d7f320e3859139ac72dd2634e
Jacob Burenstam <burenstam@gmail.com>
2013-12-04 20:56:32 +0100

    update

--- app/controllers/sensor_outputs_controller.rb
+++ app/controllers/sensor_outputs_controller.rb
@@ -2,10 +2,10 @@ class SensorOutputsController < ApplicationController
 
   def create
     so = SensorOutput.new
-    so.control_output = params[:control_output]
-    so.latency = params[:latency]
-    so.angle = params[:angle]
-    so.position = params[:position]
+    so.control_output = params[:output]
+    so.latency =        params[:latency]
+    so.angle =          params[:angle]
+    so.position =       params[:position]
     so.save!
     render text: 'Success'
   end
