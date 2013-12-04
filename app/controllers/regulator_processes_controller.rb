2e0c2bb94078f60d2892cb8741c557c6a0e7bfcf
Jacob Burenstam <burenstam@gmail.com>
2013-12-04 15:38:36 +0100

    update

--- app/controllers/regulator_processes_controller.rb
+++ app/controllers/regulator_processes_controller.rb
@@ -3,6 +3,7 @@ class RegulatorProcessesController < ApplicationController
   def index
     @config = ProcessConfig.last
     @config = ProcessConfig.new unless @config
+    @raspberry = Raspberry.last
   end
 
 end
