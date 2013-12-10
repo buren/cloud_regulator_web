771fd41d17040679c7ef4e920399f2b290eb0b3a
Jacob Burenstam <burenstam@gmail.com>
2013-12-10 15:44:48 +0100

    update

--- app/controllers/process_cycles_controller.rb
+++ app/controllers/process_cycles_controller.rb
@@ -4,17 +4,20 @@ class ProcessCyclesController < ApplicationController
     plot_points = params[:points].blank? ? 30 : params[:points].to_i
     sos = SensorOutput.last(plot_points)
     data = Array.new
+    ref_sig = ReferenceSignal.last
+
     sos.each_with_index do |so, index|
       data << {
         x:  index,
         y: so.angle,
         z: so.position,
         o: so.control_output,
-        a: so.latency
+        a: so.latency,
+        ref: ref_sig.reference_value
       }
     end
 
-    ref_sig = ReferenceSignal.last
+
     ref_sig.reference_value *= -1 if (params[:req] && params[:alt].eql?("true")) and (params[:req].to_i%5).eql?(0)
     ref_sig.save!
 
